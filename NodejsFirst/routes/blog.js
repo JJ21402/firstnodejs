d72c41af48dab769f5578b3d223c47107c496c14
JJ <pasin.pnz@mail.kmutt.ac.th>
2020-12-23 14:43:54 +0700

    [EDIT] get value from addblog and validate by using express validator

--- NodejsFirst/routes/blog.js
+++ NodejsFirst/routes/blog.js
@@ -1,12 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+// ใช้ express-validator ในการตรวจสอบค่าที่จะนำเข้าสู่ db
+const { body, validationResult } = require("express-validator");
 
+router.get("/", function (req, res, next) {
+  res.render("blog");
+});
+router.get("/add", function (req, res, next) {
+  res.render("addblog");
+});
+router.post(
+  "/add",
+  [
+    body("name", "Please input your blog name").not().isEmpty(), //name ต้องไม่เป็นค่าว่างถ้าว่างจะส่งข้อความกลับ
+    body("descript", "Please input your description").not().isEmpty(),
+    body("author", "Please input author").not().isEmpty()
+  ],
+  function (req, res, next) {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+        var errors = result.errors;
+        res.render('addblog',{errors:errors})
+    }else{
 
-router.get('/',function(req, res, next){
-    res.render('blog');
-})
-router.get('/add',function(req, res, next){
-    res.render('addblog');
-})
+    }
+  }
+);
 
 module.exports = router;
